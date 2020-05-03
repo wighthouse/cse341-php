d45701be1402ce25992f9131709a95281083b4cb
Maretta Wight <wig18001@byui.edu>
2020-05-03 02:32:04 -0600

    assignments

---
+++ web/Homepage/assignments.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>CTE341 | Web Backend Development II | Assignments</title>
+
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&Oswald&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+    <?php include 'php/header.php';?>
+    </header>
+    <main>
+        <div class="main-container">
+            <h1>Coming Soon</h1>
+            <div class="home-info">
+                
+            </div>
+        </div>
+
+    </main>
+
+    <footer>
+    <?php include 'php/footer.php';?>
+    </footer>
+</body>
+
+</html>
