5545066b3015e1a177b8d86f6b863ad5e9d14df7
Maretta Wight <wig18001@byui.edu>
2020-05-05 23:23:28 -0600

    array

--- web/team-act03/ta03final.php
+++ web/team-act03/ta03final.php
@@ -1,3 +1,7 @@
+<?php
+$major=array("CS"=>"Computer Science","WebDev"=>"Web Design and Development","CIT"=>"Computer Information Technology", "CE"=> "Computer Engineering");
+echo "Peter's major is " . $major['CE'];
+?>
 <!DOCTYPE html>
 <html lang="en-us">
 <head>
@@ -11,14 +15,14 @@
         <label for="userName">Name: <input type="text" name="userName"></label><br>
         <label for="userEmail">Email: <input type="text" name="userEmail"></label><br>
         <!--Radio list of major-->
-        <input type="radio" id="CS" name="major" value="Computer Science">
+        <!-- <input type="radio" id="CS" name="major" value="Computer Science">
         <label for="CS">Computer Science</label><br>
         <input type="radio" id="WebDev" name="major" value="Web Design and Development">
         <label for="WebDev">Web Design and Development</label><br>
         <input type="radio" id="CIT" name="major" value="Computer Information Technology">
         <label for="CIT">Computer Information Technology</label><br>
         <input type="radio" id="CE" name="major" value="Computer Engineering">
-        <label for="CE">Computer Engineering</label><br><br>
+        <label for="CE">Computer Engineering</label><br><br> -->
         <!--Which countries did the user visit?-->
         <input type="checkbox" name="places[]" id="northAmerica" value="0"><label for="northAmerica">North America</label><br />
         <input type="checkbox" name="places[]" id="southAmerica" value="1"><label for="southAmerica">South America</label><br />
