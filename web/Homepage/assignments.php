3cd41f171bbb27f8e79a85f246fe1812a41b9594
Maretta Wight <wig18001@byui.edu>
2020-05-18 13:06:20 -0600

    link

--- web/Homepage/assignments.php
+++ web/Homepage/assignments.php
@@ -23,7 +23,7 @@
                     <ul>
                         <li><a href="https://murmuring-coast-84451.herokuapp.com/shopping-cart/shop.php">Wk 3: Shopping Cart</a></li>
                         <li ><a href="https://github.com/wighthouse/cse341-php/tree/master/db">Wk 4: DB Setup</a></li>
-                        <li>Wk 5: DB Access</li>
+                        <li><a href="https://murmuring-coast-84451.herokuapp.com/team-act05/scriptures.php">Wk 5: DB Access</li>
                         <li>Wk 6: DB Update</li>
                         <li>Wk 7: Project 1</li>
                     </ul>
