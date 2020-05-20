4c8646204384acc4cf76f3cd691058d61d287d3b
Maretta Wight <wig18001@byui.edu>
2020-05-19 21:12:10 -0600

    m

--- web/team-act05/scrip-details.php
+++ web/team-act05/scrip-details.php
@@ -32,24 +32,24 @@
         return $data;
     }
 
-    function searchQuery($name, $db) {
+    function displayQuery($id, $db) {
    // $db=dbConnection();    
-    $stmt = $db->prepare('SELECT * FROM scriptures WHERE book = :name');
+    $stmt = $db->prepare('SELECT * FROM scriptures WHERE id = :id');
     //$name= '$name';
-    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
     $stmt->execute();
     $book = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $book;
     }
 
     // If the page loads as a POST request, look for this variable, and if it is set
-if(isset($_POST['bookToFind'])) {
+if(isset($_GET['id'])) {
     // This is just for testing to make sure we have the correct text
     //echo "<h1>" . $_POST['bookToFind'] . "</h1>";
     // Validate & sanitize the input
-    $searchText = validateInput($_POST['bookToFind']);
+    $searchText = validateInput($_GET['id']);
     // Now run the query to find the text in the database, and then save the results as a variable
-    $books = searchQuery($searchText, $db);
+    $books = displayQuery($searchText, $db);
   // Change the method name
   print_r($books);
   
@@ -63,7 +63,7 @@ if(isset($_POST['bookToFind'])) {
 <head>
     <meta charset="utf-8">
 
-    <title>CTE341 | Web Backend Development II | Scripture Resources</title>
+    <title>CTE341 | Web Backend Development II | Scripture Details</title>
 
     <link rel="stylesheet" href="/Homepage/css/main.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&Oswald&display=swap" rel="stylesheet">
