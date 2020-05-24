8a6e2026145c39793bd7a1e3b265ca1b9e8f7a32
Maretta Wight <wig18001@byui.edu>
2020-05-24 03:16:47 -0600

    nav

--- web/Homepage/php/header.php
+++ web/Homepage/php/header.php
@@ -4,8 +4,8 @@
             <div class="nav-container">
                 <h1>CTE 341 - Web Backend Development II</h1>
                 <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="assignments.php">Assignments</a></li>
+                    <li><a href="/Homepage/index.php">Home</a></li>
+                    <li><a href="/Homepage/assignments.php">Assignments</a></li>
                     <li><a href="/race/race-registration.php">Project</a></li>
 
 
