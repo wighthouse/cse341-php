34dc4c9eb6f6799d0816de4b55fa14a2b0b3cf9c
Maretta Wight <wig18001@byui.edu>
2020-05-26 20:41:29 -0600

    m

--- web/team-act05/scriptures2.php
+++ web/team-act05/scriptures2.php
@@ -46,27 +46,27 @@ foreach ($topics as $topic) {
         echo "</form>";   
     }
 
-    try
-    {
-      $dbUrl = getenv('DATABASE_URL');
+    // try
+    // {
+    //   $dbUrl = getenv('DATABASE_URL');
     
-      $dbOpts = parse_url($dbUrl);
+    //   $dbOpts = parse_url($dbUrl);
     
-      $dbHost = $dbOpts["host"];
-      $dbPort = $dbOpts["port"];
-      $dbUser = $dbOpts["user"];
-      $dbPassword = $dbOpts["pass"];
-      $dbName = ltrim($dbOpts["path"],'/');
+    //   $dbHost = $dbOpts["host"];
+    //   $dbPort = $dbOpts["port"];
+    //   $dbUser = $dbOpts["user"];
+    //   $dbPassword = $dbOpts["pass"];
+    //   $dbName = ltrim($dbOpts["path"],'/');
     
-      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    //   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
     
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $ex)
-    {
-      echo 'Error!: ' . $ex->getMessage();
-      die();
-    }
+    //   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // }
+    // catch (PDOException $ex)
+    // {
+    //   echo 'Error!: ' . $ex->getMessage();
+    //   die();
+    // }
  
     
 ?>
