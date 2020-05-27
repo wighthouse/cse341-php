9d709a772e445dd43ef5b6d2f2ad1638b96cca5c
Maretta Wight <wig18001@byui.edu>
2020-05-26 22:00:02 -0600

    m

--- web/team-act05/add-scriptures.php
+++ web/team-act05/add-scriptures.php
@@ -90,7 +90,7 @@ catch (Exception $ex)
 }
 
 // finally, redirect them to a new page to actually show the topics
-// header("Location: showTopics.php");
+header("Location: show-topics.php");
 
 die(); // we always include a die after redirects. In this case, there would be no
        // harm if the user got the rest of the page, because there is nothing else
