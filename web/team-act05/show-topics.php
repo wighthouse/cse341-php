49834f95063590b99f8d2712070e5efb31edcfb4
Maretta Wight <wig18001@byui.edu>
2020-05-26 21:58:32 -0600

    show topics

---
+++ web/team-act05/show-topics.php
@@ -0,0 +1,118 @@
+<?php
+    
+
+    function get_db(){
+        $db=NULL;
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     
+    }
+    catch (PDOException $ex)
+        {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+    return $db;
+    }
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>CTE341 | Web Backend Development II | Scripture Details</title>
+
+    <link rel="stylesheet" href="../Homepage/css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&Oswald&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+        <?php include 'Homepage/php/header.php'; ?>
+    </header>
+    <main>
+        <div class="main-container">
+            <h2 class="page-title">Insert Scriptures</h2>
+         
+       
+       <?php
+
+
+try
+{
+	// For this example, we are going to make a call to the DB to get the scriptures
+	// and then for each one, make a separate call to get its topics.
+	// This could be done with a single query (and then more processing of the resultset
+	// afterward) as follows:
+
+	//	$statement = $db->prepare('SELECT book, chapter, verse, content, t.name FROM scripture s'
+	//	. ' INNER JOIN scripture_topic st ON s.id = st.scriptureId'
+	//	. ' INNER JOIN topic t ON st.topicId = t.id');
+
+
+	// prepare the statement
+	$statement = $db->prepare('SELECT id, book, chapter, verse, content FROM scriptures');
+	$statement->execute();
+
+	// Go through each result
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
+		echo '<p>';
+		echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':';
+		echo $row['verse'] . '</strong>' . ' - ' . $row['content'];
+		echo '<br />';
+		echo 'Topics: ';
+
+		// get the topics now for this scripture
+		$stmtTopics = $db->prepare('SELECT name FROM topics t'
+			. ' INNER JOIN scripture_topic st ON st.topicId = t.id'
+			. ' WHERE st.scripture_id = :scripture_id');
+
+		$stmtTopics->bindValue(':scripture_id', $row['id']);
+		$stmtTopics->execute();
+
+		// Go through each topic in the result
+		while ($topicRow = $stmtTopics->fetch(PDO::FETCH_ASSOC))
+		{
+			echo $topicRow['topic'] . ' ';
+		}
+
+		echo '</p>';
+	}
+
+
+}
+catch (PDOException $ex)
+{
+	echo "Error with DB. Details: $ex";
+	die();
+}
+
+?>
+ </div>
+           
+
+           </main>
+       
+           <footer>
+           
+           </footer>
+       </body>
+       
+       </html>
