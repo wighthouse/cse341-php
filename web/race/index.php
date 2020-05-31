31096c115f7f0c73e8644a4e5049341b201f36c6
Maretta Wight <wig18001@byui.edu>
2020-05-31 01:28:46 -0600

    modify stuff

--- web/race/index.php
+++ web/race/index.php
@@ -1,6 +1,6 @@
-<?php 
-  require_once('functions.php');
-   $db = get_db();
+<?php
+require_once('functions.php');
+$db = get_db();
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,38 +23,33 @@
         <div class="main-container">
             <h2 class="page-title">Hennepin County Peanuts Run</br>Registration Information Page</h2>
 
-          
+
         </div>
         <div class="main-form search">
-        <h3>Search for Race Participants by event.</h3>
-        
-        <?php 
-        $searchBox = search();
-        echo $searchBox;
-        ?>
+            <h3>Search for Race Participants by event.</h3>
+
+            <?php
+            $searchBox = search();
+            echo $searchBox;
+            ?>
         </div>
 
         <div class="main-form register">
-        <h3>Register for an event.</h3>
-        
-        <?php 
-        $searchBox = search();
-        echo $searchBox;
-        ?>
+            <h3>Register for an event.</h3>
+
+            <a href="race-registration.php" class="button">Go to Registration Page</a>
         </div>
 
         <div class="main-form modify">
-        <h3>Modify your Registration.</h3>
-        
-        <?php 
-        $searchBox = search();
-        echo $searchBox;
-        ?>
+            <form class="formContainer" action="race-modify.php" method="post">
+                <h3 class="formHeading">Modify your Registration</h3>
+                <label class="above">Confirmation Id*<input type="text" name="confirmation_id" required></label>
+                <input type="submit" name="submit" class="button" value="Modify">
         </div>
     </main>
 
     <footer>
-    <?php include '../Homepage/php/footer.php';?>
+        <?php include '../Homepage/php/footer.php'; ?>
     </footer>
 </body>
 
