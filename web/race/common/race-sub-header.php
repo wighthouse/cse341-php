dd800ba60e826cffeb76dd8a3533a71affe6cb2a
Maretta Wight <wig18001@byui.edu>
2020-06-06 14:37:08 -0600

    sub-header

---
+++ web/race/common/race-sub-header.php
@@ -0,0 +1,14 @@
+<div class="sub-border"></div>
+        
+        <nav class="sub-header-nav">
+            <div class="sub-nav-container">
+                <h1>Hennepin County Peanuts Run</h1>
+                <ul>
+                    <li><a href="/race/index.php">Race Home</a></li>
+                    <li><a href="/race/race-registration.php">Register</a></li>
+                    <li><a href="/race/race-modify.php">Modify</a></li>
+
+
+                </ul>
+            </div>
+        </nav>
