4713bf78eeabff2f7afb2b5544dfd9010c0a9742
Maretta Wight <wig18001@byui.edu>
2020-06-06 16:02:33 -0600

    div

--- web/race/common/race-header.php
+++ web/race/common/race-header.php
@@ -1,23 +1,25 @@
 <div class="top-border">
-<nav class="header-nav">
-            <div class="nav-container">
-                <h1>CTE 341 - Web Backend Development II</h1>
-                <ul>
-                    <li><a href="/Homepage/index.php">Home</a></li>
-                    <li><a href="/Homepage/assignments.php">Assignments</a></li>
-                    <li><a href="/race/index.php">Project</a></li>
-                </ul>
-            </div>
+    <nav class="header-nav">
+        <div class="nav-container">
+            <h1>CTE 341 - Web Backend Development II</h1>
+            <ul>
+                <li><a href="/Homepage/index.php">Home</a></li>
+                <li><a href="/Homepage/assignments.php">Assignments</a></li>
+                <li><a href="/race/index.php">Project</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
 <div class="image-container"></div>
-        </nav></div>
 <div class="sub-border">
-        <nav class="sub-header-nav">
-            <div class="sub-nav-container">
-                <h1>Hennepin County Peanuts Run</h1>
-                <ul>
-                    <li><a href="/race/index.php">Race Home</a></li>
-                    <li><a href="/race/race-registration.php">Register</a></li>
-                    <li><a href="/race/race-modify.php">Modify</a></li>
-                </ul>
-            </div>
-        </nav></div>
+    <nav class="sub-header-nav">
+        <div class="sub-nav-container">
+            <h1>Hennepin County Peanuts Run</h1>
+            <ul>
+                <li><a href="/race/index.php">Race Home</a></li>
+                <li><a href="/race/race-registration.php">Register</a></li>
+                <li><a href="/race/race-modify.php">Modify</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
