a6f9cf1c3e8bf6b9517faf0c9dbffb5b883f5f56
Maretta Wight <wig18001@byui.edu>
2020-06-06 16:24:17 -0600

    last change footer for now

--- web/race/common/race-footer.php
+++ web/race/common/race-footer.php
@@ -1,6 +1,6 @@
-<div class="footer-container"><div class="image-container">
+<div class="image-container"></div><div class="footer-container">
 <?php
 echo "<p>Copyright &copy; " . date("Y") . " Maretta Wight</p>";
 ?>
-</div>
+
 </div>
